6f1c800867c07ddc29fd9d6b7e564ddd680cf158
elise <Elisenovaes10@gmail.com>
2024-01-03 10:19:28 -0300

    Adição da pasta 'Listas'

---
+++ 06. listas(arrays)/aula.js
@@ -0,0 +1,25 @@
+const alunos = ['joão', 'vitor', 'marina'];
+//console.log(alunos);
+
+alunos.push('elise');
+
+alunos[4] = 'gabriel';
+
+//console.log(alunos.pop());
+//Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+
+//console.log(alunos.shift());
+//Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+
+//como funciona um for
+const nome = 'elise novaes'
+for (let i = 0; i < nome.length; i++){
+    console.log(nome[i]);   
+}
+
+
+
+
+
+
+
---
+++ 06. listas(arrays)/tabuada.js
@@ -0,0 +1,5 @@
+
+const n = 8;
+for (let i = 1; i <= 10; i++) {
+    console.log(i*n);
+}
